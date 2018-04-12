7e50dabac15fda7701fb001e8b3610a43c7e024a
hanmo <dlgksah@gmail.com>
2018-04-12 22:12:30 +0900

    민지님 RecyclerView 입니다!!

---
+++ app/src/main/java/com/example/minzee/minzeehomwork/recyclerView/MinzeeItem.java
@@ -0,0 +1,28 @@
+package com.example.minzee.minzeehomwork.recyclerView;
+
+/**
+ * Created by hanmo on 2018. 4. 12..
+ */
+
+public class MinzeeItem {
+
+    private int minzeeAge;
+    private String minzeeName;
+
+    public int getMinzeeAge() {
+        return minzeeAge;
+    }
+
+    public void setMinzeeAge(int minzeeAge) {
+        this.minzeeAge = minzeeAge;
+    }
+
+    public String getMinzeeName() {
+        return minzeeName;
+    }
+
+    public void setMinzeeName(String minzeeName) {
+        this.minzeeName = minzeeName;
+    }
+
+}
